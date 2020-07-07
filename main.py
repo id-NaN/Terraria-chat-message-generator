c8bb523a85e2fd98aba179ec6f4d4d97a5d0efee
unknown <morgan.kuhles@gmail.com>
2020-07-07 21:49:50 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -7,31 +7,10 @@ import eel
 
 
 
-eel.init("web")
-eel.start("main.html")
-
-
-
 def number_place_minimum(number, places):
 	return (places - len(number)) * "0" + number
 
 
 
-# class Item:
-# 	def __init__(self, dictionary):
-# 		[setattr(self, key, value) for key, value in dictionary.items()]
-
-# 	def __str__(self):
-# 		return "\n".join([
-# 				"id:" + "\t" * 2 + str(self.item_id),
-# 				"name:" + "\t" + self.item_name,
-# 				"wiki:" + "\t" + self.wiki_url
-# 				])
-
-
-
-# with open("C:\\Users\\morga\\Desktop\\Projects\\terraria chat generator\\terraria_items.json", "r") as items_raw:
-# 	items_list = json.load(items_raw)
-# items = [Item(dictionary) for dictionary in items_list]
-# del items_raw
-# del items_list
+eel.init("web")
+eel.start("main.html")
